bc23afc565cc062cac0e62bfe4a45b1de32be982
Ahmad Raza <ahmadrazabhatti30@gmail.com>
2022-05-07 09:30:54 +0500

    Blogs Page Setup

---
+++ pages/blogs.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Blogs() {
+  return (
+    <div className="mt-24">
+      <h2 className="text-dark-green text-4xl font-bold text-center my-3">
+        Latest Blogs
+      </h2>
+    </div>
+  );
+}
