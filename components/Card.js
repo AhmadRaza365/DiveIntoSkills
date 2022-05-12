4699e6584c8a2751b549568ed013564b563a34b5
Ahmad Raza <ahmadrazabhatti30@gmail.com>
2022-05-12 08:05:01 +0500

    posts setup

--- components/Card.js
+++ components/Card.js
@@ -16,7 +16,7 @@ export default function Card() {
         <div className="bg-dark-green relative -top-16 left-0 text-yellow w-fit px-5 py-2 rounded-r-full">Category</div>
       <div className="px-4 pb-4 -mt-8">
         <h2 className="text-dark-green font-bold text-xl cursor-pointer ">
-      <Link href="/work">
+      <Link href="/posts/abc">
           Lorem ipsum dolor sit amet consect adipisicing elit. Laboriosam,
           ab?
         </Link>
---
+++ pages/posts/[post].js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Posts() {
+  return (
+    <main className="mt-24">
+      <h2 className="text-dark-green text-4xl font-bold text-center my-3">
+        Posts
+      </h2>
+    </main>
+  );
+}
