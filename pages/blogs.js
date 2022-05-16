664b4ded7d886d41ef821daf990f58274b03f2e6
Ahmad Raza <ahmadrazabhatti30@gmail.com>
2022-05-16 17:53:53 +0500

    Optimising Card For Props & Blogs Page fixing

--- pages/blogs.js
+++ pages/blogs.js
@@ -5,35 +5,25 @@ import matter from "gray-matter";
 import Link from "next/link";
 import Card from "../components/Card";
 
-export default function Blogs({posts}) {
+export default function Blogs({ posts }) {
   return (
     <main className="mt-24">
       <h2 className="text-dark-green text-4xl font-bold text-center my-3">
         Latest Blogs
       </h2>
 
-      {
-        posts.map((post, index) => (
-          <h3 key={post.slug}>{post.postMeta.title} -- {post.slug}</h3>
-        ))
-      }
-
-
-
       <section className="flex flex-col items-center my-5">
         <div className="flex flex-wrap justify-center items-center">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        {posts.map((post, index) => (
+        <Card
+          key={post.slug}
+          title = {post.postMeta.title}
+          slug={post.slug}
+          image={post.postMeta.cover_image}
+          description={post.postMeta.excerpt}
+          date={post.postMeta.date}
+        />
+      ))}
         </div>
       </section>
     </main>
@@ -43,23 +33,26 @@ export default function Blogs({posts}) {
 // Fetching Posts
 export async function getStaticProps() {
   const files = fs.readdirSync(path.join("postsData"));
-  
-  const posts = files.map(filename => {
+
+  const posts = files.map((filename) => {
     const slug = filename.replace(".md", "");
 
-    const frontmatter = fs.readFileSync(path.join('postsData', filename), 'utf8');
+    const frontmatter = fs.readFileSync(
+      path.join("postsData", filename),
+      "utf8"
+    );
 
-    const {data:postMeta} = matter(frontmatter);
+    const { data: postMeta } = matter(frontmatter);
 
     return {
       slug,
-      postMeta
-    }
-  })
+      postMeta,
+    };
+  });
 
   return {
     props: {
-      posts
+      posts,
     },
   };
 }
