786b09aaef098675ef46b39472e0dde66370df22
Ahmad Raza <ahmadrazabhatti30@gmail.com>
2022-05-16 17:11:49 +0500

    Fetching Posts setup

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,14 +1,26 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
 import Link from "next/link";
 import Card from "../components/Card";
 
-export default function Blogs() {
+export default function Blogs({posts}) {
   return (
     <main className="mt-24">
       <h2 className="text-dark-green text-4xl font-bold text-center my-3">
         Latest Blogs
       </h2>
+
+      {
+        posts.map((post, index) => (
+          <h3 key={post.slug}>{post.postMeta.title} -- {post.slug}</h3>
+        ))
+      }
+
+
+
       <section className="flex flex-col items-center my-5">
-        
         <div className="flex flex-wrap justify-center items-center">
           <Card />
           <Card />
@@ -23,8 +35,31 @@ export default function Blogs() {
           <Card />
           <Card />
         </div>
-        
       </section>
     </main>
   );
+}
+
+// Fetching Posts
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("postsData"));
+  
+  const posts = files.map(filename => {
+    const slug = filename.replace(".md", "");
+
+    const frontmatter = fs.readFileSync(path.join('postsData', filename), 'utf8');
+
+    const {data:postMeta} = matter(frontmatter);
+
+    return {
+      slug,
+      postMeta
+    }
+  })
+
+  return {
+    props: {
+      posts
+    },
+  };
 }
---
+++ postsData/test.md
@@ -0,0 +1,14 @@
+---
+title: 'Test Post'
+date: 'May 16, 2022'
+excerpt: 'This is a test post.'
+cover_image: '/images/postImages/testimage.png'
+---
+## This is a test post.
+This is a test post. I am testing the markdown parser.
+
+* item1
+* item2
+* item3
+* item4
+* item5
